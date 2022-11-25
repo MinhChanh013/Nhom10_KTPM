555a9a029d162803a7db302a358e4068a87b0040
Nguyen Minh Chanh <nguyenminhchanh1910@gmail.com>
2022-11-25 09:22:44 +0700

    update service product & registry

---
+++ product_type/src/main/java/com/kttkpm/product_type/model/ProductType.java
@@ -0,0 +1,22 @@
+package com.kttkpm.product_type.model;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.redis.core.RedisHash;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "product_types")
+public class ProductType{
+	@Id
+	private ObjectId productTypeId;
+	private String productTypeName;
+
+
+}
